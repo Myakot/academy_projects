f76c1cc18a47d1b61c33bb05efdf78c15863bb1d
Myakot <cstepan@yandex.ru>
2024-11-16 22:31:55 +0700

    Corrected the entire code for 6_7 task - removed Flask usage from it and made a lightweight WSGI app

--- 6_7_ASGI_WSGI/main.py
+++ 6_7_ASGI_WSGI/main.py
@@ -1,14 +1,21 @@
-from flask import Flask, jsonify
 import requests
+import json
+from wsgiref.simple_server import make_server
 
-app = Flask(__name__)
-
-@app.route('/USD', methods=['GET'])
-def get_exchange_rate():
-    url = 'https://api.exchangerate-api.com/v4/latest/USD'
-    response = requests.get(url)
-    data = response.json()
-    return jsonify(data)
+def app(environ, start_response):
+    if environ['REQUEST_METHOD'] == 'GET' and environ['PATH_INFO'] == '/USD':
+        url = 'https://api.exchangerate-api.com/v4/latest/USD'
+        response = requests.get(url)
+        data = response.json()
+        headers = [('Content-Type', 'application/json')]
+        start_response('200 OK', headers)
+        return [json.dumps(data).encode('utf-8')]
+    else:
+        headers = [('Content-Type', 'text/plain')]
+        start_response('404 Not Found', headers)
+        return [b'Not Found']
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    server = make_server('localhost', 8000, app)
+    print('Сервер запущен, чтобы протестировать: http://localhost:8000/USD')
+    server.serve_forever()
